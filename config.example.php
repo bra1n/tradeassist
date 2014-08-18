f9682c64c34c53cee92eb4ba816453bfe353a9ab
Steffen <steffen@baumgart.biz>
2014-08-18 10:27:06 +0200

    use teh new api

--- config.example.php
+++ config.example.php
@@ -1,10 +1,14 @@
 <?php
 # add the appropriate values and rename this file to config.php
 
-define("ENV",         ""); # use "development" for local testing
-define("DB_SERVER",   ""); # database hostname
-define("DB_USERNAME", ""); # database username
-define("DB_PASSWORD", ""); # database password
-define("DB_DATABASE", ""); # database name
-define("MKMAPI",      ""); # URL to the MKM API
-define("USAPI",       ""); # URL to the US API
+define("ENV",             ""); # use "development" for local testing
+define("DB_SERVER",       ""); # database hostname
+define("DB_USERNAME",     ""); # database username
+define("DB_PASSWORD",     ""); # database password
+define("DB_DATABASE",     ""); # database name
+define("MKMAPI",          "https://www.mkmapi.eu/ws/v1.1/"); # URL to the MKM API
+define("MKMAPPTOKEN",     ""); # MKM Api App Token
+define("MKMAPPSECRET",    ""); # MKM Api App Secret
+define("MKMACCESSTOKEN",  ""); # MKM Api Access Token
+define("MKMACCESSSECRET", ""); # MKM Api Access Secret
+define("USAPI",           ""); # URL to the US API
--- functions.php
+++ functions.php
@@ -196,10 +196,9 @@ function getCardByIdFromMKM($id) {
   $card = new stdClass();
   $card->error = "";
   $card->id = $id;
-  $xml = @file_get_contents(MKMAPI."product/$id");
+  $xml = queryMKMAPI("product/$id");
   if(!$xml) $card->error = "error loading card";
   else {
-    $xml = simplexml_load_string($xml);
     if(isset($xml->product) AND intval($xml->product->category->idCategory) == 1) {
       $card->name = str_replace("Æ","AE",$xml->product->name[0]->productName);
       $card->name_de = str_replace("Æ","AE",$xml->product->name[2]->productName);
@@ -279,26 +278,8 @@ function getOffersByIdFromMKM($id) {
 	$offers = array();
 	$id = intval($id);
 
-  $xml = @file_get_contents(MKMAPI."articles/$id");
-  if($xml) {
-    $xml = simplexml_load_string($xml);
-    foreach($xml as $article) $offers[] = $article;
-    if(strpos($http_response_header[0],"206 Partial Content") !== false) {
-      $total = 0;
-      foreach($http_response_header as $headerline) {
-        if(preg_match("~Range: \d+-\d+/(\d+)~i",$headerline,$match)) {
-          $total = $match[1];
-          break;
-        }
-      }
-      for($x = 101;$x <= $total; $x+=100) {
-        $xml = @file_get_contents(MKMAPI."articles/$id/$x");
-        $xml = simplexml_load_string($xml);
-        foreach($xml as $article) $offers[] = $article;
-      }
-    }
-  }
-  foreach($offers as $index=>$offer) {
+  $xml = queryMKMAPI("articles/$id");
+  foreach($xml->article as $offer) {
     $isAltered = $offer->isAltered == "true" ? 1:0;
     $isFoil = $offer->isFoil == "true" ? 1:0;
     $isPlayset = $offer->isPlayset == "true" ? 1:0;
@@ -310,13 +291,13 @@ function getOffersByIdFromMKM($id) {
     $seller = strval($offer->seller->username);
     $level = intval($offer->seller->reputation); # 0 = best, 4+ = worst
     $warning = intval($offer->seller->riskGroup); # 0 = no warning, 1 = new seller, 2 = big warning
-    $rating = 0; # todo replace with real value once available
-    $speed = 0; # todo replace with real value once available
+    $rating = intval($offer->seller->reputation);
+    $speed = intval($offer->seller->shipsFast); # todo replace with real value once available
     if($isPlayset) {
       $count = $count * 4;
       $price = $price / 4;
     }
-    $offers[$index] = array(
+    $offers[] = array(
       "altered" => $isAltered,
       "foil"    => $isFoil,
       "playset" => $isPlayset,
@@ -333,4 +314,146 @@ function getOffersByIdFromMKM($id) {
     );
   }
 	return $offers;
+}
+
+function queryMKMAPI($url) {
+  /**
+   * Declare and assign all needed variables for the request and the header
+   *
+   * @var $method string Request method
+   * @var $url string Full request URI
+   * @var $appToken string App token found at the profile page
+   * @var $appSecret string App secret found at the profile page
+   * @var $accessToken string Access token found at the profile page (or retrieved from the /access request)
+   * @var $accessSecret string Access token secret found at the profile page (or retrieved from the /access request)
+   * @var $nonce string Custom made unique string, you can use uniqid() for this
+   * @var $timestamp string Actual UNIX time stamp, you can use time() for this
+   * @var $signatureMethod string Cryptographic hash function used for signing the base string with the signature, always HMAC-SHA1
+   * @var version string OAuth version, currently 1.0
+   */
+  $method             = "GET";
+  $url                = MKMAPI.$url;
+  $appToken           = MKMAPPTOKEN;
+  $appSecret          = MKMAPPSECRET;
+  $accessToken        = MKMACCESSTOKEN;
+  $accessSecret       = MKMACCESSSECRET;
+  $nonce              = uniqid();
+  $timestamp          = time();
+  $signatureMethod    = "HMAC-SHA1";
+  $version            = "1.0";
+
+  /**
+   * Gather all parameters that need to be included in the Authorization header and are know yet
+   *
+   * @var $params array|string[] Associative array of all needed authorization header parameters
+   */
+  $params             = array(
+    'realm'                     => $url,
+    'oauth_consumer_key'        => $appToken,
+    'oauth_token'               => $accessToken,
+    'oauth_nonce'               => $nonce,
+    'oauth_timestamp'           => $timestamp,
+    'oauth_signature_method'    => $signatureMethod,
+    'oauth_version'             => $version,
+  );
+
+  /**
+   * Start composing the base string from the method and request URI
+   *
+   * @var $baseString string Finally the encoded base string for that request, that needs to be signed
+   */
+  $baseString         = strtoupper($method) . "&";
+  $baseString        .= rawurlencode($url) . "&";
+
+  /*
+   * Gather, encode, and sort the base string parameters
+   */
+  $encodedParams      = array();
+  foreach ($params as $key => $value)
+  {
+    if ("realm" != $key)
+    {
+      $encodedParams[rawurlencode($key)] = rawurlencode($value);
+    }
+  }
+  ksort($encodedParams);
+
+  /*
+   * Expand the base string by the encoded parameter=value pairs
+   */
+  $values             = array();
+  foreach ($encodedParams as $key => $value)
+  {
+    $values[] = $key . "=" . $value;
+  }
+  $paramsString       = rawurlencode(implode("&", $values));
+  $baseString        .= $paramsString;
+
+  /*
+   * Create the signingKey
+   */
+  $signatureKey       = rawurlencode($appSecret) . "&" . rawurlencode($accessSecret);
+
+  /**
+   * Create the OAuth signature
+   * Attention: Make sure to provide the binary data to the Base64 encoder
+   *
+   * @var $oAuthSignature string OAuth signature value
+   */
+  $rawSignature       = hash_hmac("sha1", $baseString, $signatureKey, true);
+  $oAuthSignature     = base64_encode($rawSignature);
+
+  /*
+   * Include the OAuth signature parameter in the header parameters array
+   */
+  $params['oauth_signature'] = $oAuthSignature;
+
+  /*
+   * Construct the header string
+   */
+  $header             = "Authorization: OAuth ";
+  $headerParams       = array();
+  foreach ($params as $key => $value)
+  {
+    $headerParams[] = $key . "=\"" . $value . "\"";
+  }
+  $header            .= implode(", ", $headerParams);
+
+  /*
+   * Get the cURL handler from the library function
+   */
+  $curlHandle         = curl_init();
+
+  /*
+   * Set the required cURL options to successfully fire a request to MKM's API
+   *
+   * For more information about cURL options refer to PHP's cURL manual:
+   * http://php.net/manual/en/function.curl-setopt.php
+   */
+  curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curlHandle, CURLOPT_URL, $url);
+  curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array($header));
+  curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
+
+  /**
+   * Execute the request, retrieve information about the request and response, and close the connection
+   *
+   * @var $content string Response to the request
+   * @var $info array Array with information about the last request on the $curlHandle
+   */
+  $content            = curl_exec($curlHandle);
+  $info               = curl_getinfo($curlHandle);
+  curl_close($curlHandle);
+
+  /*
+   * Convert the response string into an object
+   *
+   * If you have chosen XML as response format (which is standard) use simplexml_load_string
+   * If you have chosen JSON as response format use json_decode
+   *
+   * @var $decoded \SimpleXMLElement|\stdClass Converted Object (XML|JSON)
+   */
+  // $decoded            = json_decode($content);
+  $decoded            = simplexml_load_string($content);
+  return $decoded;
 }
